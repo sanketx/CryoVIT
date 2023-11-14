ad6f69d8e14deb3de29610c1fbc616545e9d57e4
Sanket Gupte <6501744+sanketx@users.noreply.github.com>
2023-11-14 01:21:39 -0800

    refactored dino feature extraction code

--- cryovit/data_proc/dino_features.py
+++ cryovit/data_proc/dino_features.py
@@ -1,40 +1,42 @@
 import os
-import sys
+import shutil
+from pathlib import Path
 
 import h5py
 import numpy as np
 import pandas as pd
 import torch
+from numpy.typing import NDArray
 from rich.progress import track
+from torch import nn
 from torch.utils.data import DataLoader
 
-
-from .. import config
-from cryovit.src.datasets import VITDataset
+from cryovit.config import Sample
+from cryovit.datasets import VITDataset
 
 
 torch.set_float32_matmul_precision("high")
+dino_model = ("facebookresearch/dinov2", "dinov2_vitg14")
 
 dataloader_params = {
     "batch_size": None,
     "pin_memory": True,
-    "num_workers": 1,
+    "num_workers": 0,
 }
-
-dino_vit_S14 = ("facebookresearch/dinov2", "dinov2_vits14")
-dino_vit_B14 = ("facebookresearch/dinov2", "dinov2_vitb14")
-dino_vit_L14 = ("facebookresearch/dinov2", "dinov2_vitl14")
-dino_vit_g14 = ("facebookresearch/dinov2", "dinov2_vitg14")
 
 
 @torch.inference_mode()
-def dino_features(x, model, batch_size=128):
-    x["input"] = x["input"].cuda()
-    w, h = np.array(x["input"].shape[2:]) // 14
+def dino_features(
+    data: torch.Tensor,
+    model: nn.Module,
+    batch_size: int,
+) -> NDArray[np.float16]:
+    data = data.cuda()
+    w, h = np.array(data.shape[2:]) // 14
     all_features = []
 
-    for i in range(0, len(x["input"]), batch_size):
-        vec = x["input"][i : i + batch_size]
+    for i in range(0, len(data), batch_size):
+        vec = data[i : i + batch_size]
         features = model.forward_features(vec)["x_norm_patchtokens"]
         features = features.reshape(features.shape[0], w, h, -1)
         features = features.permute([3, 0, 1, 2]).contiguous()
@@ -44,31 +46,44 @@ def dino_features(x, model, batch_size=128):
     return np.concatenate(all_features, axis=1)
 
 
-def save_data(features, x):
-    dst_dir = os.path.join(config.DINO_TOMO_DIR, x["sample"])
+def save_data(
+    features: NDArray[np.float16],
+    tomo_name: str,
+    src_dir: Path,
+    dst_dir: Path,
+) -> None:
     os.makedirs(dst_dir, exist_ok=True)
-    tomo_path = os.path.join(dst_dir, x["tomo_name"])
+    shutil.copy(src_dir / tomo_name, dst_dir)
 
-    with h5py.File(tomo_path, "w") as fh:
-        fh.create_dataset("data", data=x["data"])
-        fh.create_dataset("mito", data=x["mito"])
-        fh.create_dataset("granule", data=x["granule"])
+    with h5py.File(dst_dir / tomo_name, "r+") as fh:
         fh.create_dataset("dino_features", data=features)
 
 
-if __name__ == "__main__":
-    sample = sys.argv[1]
-    split_file = os.path.join(config.EXP_DIR, "splits.csv")
-    record_df = pd.read_csv(split_file)
-    record_df = record_df[record_df["sample"] == sample]
+def process_sample(
+    dino_dir: Path,
+    data_dir: Path,
+    feature_dir: Path,
+    batch_size: int,
+    sample: Sample,
+    **kwargs,
+) -> None:
+    src_dir = data_dir / "tomograms" / sample.name
+    dst_dir = feature_dir / sample.name
 
-    records = [row._asdict() for row in record_df.itertuples()]
-    dataloader = DataLoader(VITDataset(records), **dataloader_params)
+    record_file = data_dir / "csv" / f"{sample.name}.csv"
+    records = pd.read_csv(record_file)["tomo_name"]
 
-    torch.hub.set_dir(config.DINO_DIR)
-    model = torch.hub.load(*dino_vit_g14).cuda()
+    dataset = VITDataset(records, root=src_dir)
+    dataloader = DataLoader(dataset, **dataloader_params)
+
+    torch.hub.set_dir(dino_dir)
+    model = torch.hub.load(*dino_model, verbose=False).cuda()
     model.eval()
 
-    for x in track(dataloader, description="[green]Computing features"):
-        features = dino_features(x, model)
-        save_data(features, x)
+    for i, x in track(
+        enumerate(dataloader),
+        description="[green]Computing features",
+        total=len(dataloader),
+    ):
+        features = dino_features(x, model, batch_size)
+        save_data(features, records[i], src_dir, dst_dir)
