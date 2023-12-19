241f5b264d8f4933ef515d8dab837d967e38293e
Sanket Gupte <6501744+sanketx@users.noreply.github.com>
2023-12-19 02:26:42 -0800

    added slurm scripts for single sample experiments

---
+++ slurm_scripts/single_sample_job.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sample=$1
+split_id=$2
+model=$3
+label_key=$4
+
+env_dir=/tmp/$USER/"$(uuidgen)"
+mkdir -p $env_dir
+tar -xf ~/projects/libs/cryovit_env.tar -C $env_dir
+
+$env_dir/cryovit_env/bin/python -m \
+    cryovit.train_model \
+    model=$model \
+    label_key=$label_key \
+    exp_name="single_sample_${model}_${label_key}" \
+    dataset=single \
+    dataset.sample=$sample \
+    dataset.split_id=$split_id
+
+rm -rf $env_dir
