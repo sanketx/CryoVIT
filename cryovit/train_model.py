cbce15394915dffae9ccc72a0d13ce07b3e6122f
Sanket Gupte <6501744+sanketx@users.noreply.github.com>
2024-04-15 03:15:54 -0700

    added docstrings

--- cryovit/train_model.py
+++ cryovit/train_model.py
@@ -3,7 +3,6 @@
 import logging
 import sys
 import warnings
-from typing import Any
 
 import hydra
 from omegaconf import OmegaConf
@@ -16,6 +15,17 @@ warnings.simplefilter("ignore")
 
 
 def validate_config(cfg: TrainModelConfig) -> None:
+    """Validates the configuration for training segmentation models on CryoET data.
+
+    Checks if all necessary parameters are present in the configuration. Logs an error and exits if any
+    required parameters are missing.
+
+    Args:
+        cfg (TrainModelConfig): The configuration object containing settings for model training.
+
+    Raises:
+        SystemExit: If any configuration parameters are missing, terminating the script.
+    """
     missing_keys = OmegaConf.missing_keys(cfg)
     error_msg = ["The following parameters were missing from train_model.yaml"]
 
@@ -35,6 +45,17 @@ def validate_config(cfg: TrainModelConfig) -> None:
     version_base="1.2",
 )
 def main(cfg: TrainModelConfig) -> None:
+    """Main function to orchestrate the training of segmentation models.
+
+    Validates the provided configuration, then initializes and runs the training process using the
+    specified settings. Catches and logs any exceptions that occur during training.
+
+    Args:
+        cfg (TrainModelConfig): Configuration object loaded from train_model.yaml.
+
+    Raises:
+        BaseException: Captures and logs any exceptions that occur during the training process.
+    """
     validate_config(cfg)
 
     try:
