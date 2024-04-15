ac125f8ca5cd9d343a9e3e76c3aa1d71cde38e85
Sanket Gupte <6501744+sanketx@users.noreply.github.com>
2024-04-15 03:08:38 -0700

    added docstrings to config dataclasses

--- cryovit/config.py
+++ cryovit/config.py
@@ -1,4 +1,5 @@
 """Config file for CryoVIT experiments."""
+
 from dataclasses import dataclass
 from dataclasses import field
 from enum import Enum
@@ -32,6 +33,18 @@ Sample = Enum("Sample", samples)
 
 @dataclass
 class DinoFeaturesConfig:
+    """Configuration for managing Dino features within CryoVIT experiments.
+
+    Attributes:
+        dino_dir (Path): Path to the DINOv2 foundation model.
+        data_dir (Path): Directory containing tomograms and CSV files.
+        feature_dir (Path): Destination to save the generated DINOv2 features.
+        batch_size (int): Batch size: number of slices in one batch.
+        sample (Sample): Enum representing a specific sample under study.
+        all_samples (str): Comma-separated string of all sample names.
+        cryovit_root (Optional[Path]): Root directory for the CryoVIT package.
+    """
+
     dino_dir: Path
     data_dir: Path
     feature_dir: Path
@@ -43,6 +56,15 @@ class DinoFeaturesConfig:
 
 @dataclass
 class Model:
+    """Base class for model configurations used in CryoVIT experiments.
+
+    Attributes:
+        lr (float): Learning rate for the model training.
+        weight_decay (float): Weight decay (L2 penalty) rate. Default is 1e-3.
+        losses (Tuple[Dict]): Configuration for loss functions used in training.
+        metrics (Tuple[Dict]): Configuration for metrics used during model evaluation.
+    """
+
     lr: float
     weight_decay: float = 1e-3
     losses: Tuple[Dict] = (dict(_target_="cryovit.models.losses.DiceLoss"),)
@@ -53,12 +75,43 @@ class Model:
 
 @dataclass
 class CryoVIT(Model):
+    """Configuration for the CryoVIT model.
+
+    Attributes:
+        lr (float): Learning rate for the CryoVIT model, default set to 1e-4.
+        _target_ (str): Class identifier for instantiating a CryoVIT model.
+    """
+
     lr: float = 1e-4
     _target_: str = "cryovit.models.CryoVIT"
 
 
 @dataclass
+class UNet3D(Model):
+    """Configuration for the UNet3D model.
+
+    Attributes:
+        lr (float): Learning rate for the UNet3D model, default set to 3e-3.
+        _target_ (str): Class identifier for instantiating a UNet3D model.
+    """
+
+    lr: float = 3e-3
+    _target_: str = "cryovit.models.UNet3D"
+
+
+@dataclass
 class Trainer:
+    """Base class for trainer configurations used in CryoVIT experiments.
+
+    Attributes:
+        accelerator (str): Type of hardware acceleration ('gpu' for this configuration).
+        devices (str): Number of devices to use for training.
+        precision (str): Precision configuration for training (e.g., '16-mixed').
+        callbacks (Optional[List]): List of callback functions for training sessions.
+        enable_checkpointing (bool): Flag to enable or disable model checkpointing.
+        _target_ (str): Class identifier for instantiating a Trainer object.
+    """
+
     accelerator: str = "gpu"
     devices: str = 1
     precision: str = "16-mixed"
@@ -69,6 +122,15 @@ class Trainer:
 
 @dataclass
 class TrainerFit(Trainer):
+    """Specific configuration for fitting (training) models in CryoVIT experiments.
+
+    Attributes:
+        max_epochs (int): Maximum number of training epochs.
+        logger (Optional[List]): Logging configuration for training process.
+        log_every_n_steps (int): Interval of logging within the training process.
+        num_sanity_val_steps (int): Number of validation steps to perform at start for sanity check.
+    """
+
     max_epochs: int = 50
     logger: Optional[List] = None
 
@@ -78,17 +140,38 @@ class TrainerFit(Trainer):
 
 @dataclass
 class TrainerEval(Trainer):
+    """Configuration for model evaluation in CryoVIT experiments.
+
+    Attributes:
+        logger (bool): Flag to enable or disable logging during evaluation.
+        enable_model_summary (bool): Flag to enable or disable generation of model summaries.
+    """
+
     logger: bool = False
     enable_model_summary: bool = False
 
 
 @dataclass
 class Dataset:
+    """Base class for dataset configurations in CryoVIT experiments.
+
+    Attributes:
+        _partial_ (bool): Flag to indicate this is a partial configuration.
+    """
+
     _partial_: bool = True
 
 
 @dataclass
 class SingleSample(Dataset):
+    """Configuration for a dataset involving a single sample in CryoVIT experiments.
+
+    Attributes:
+        split_id (Optional[int]): Optional split ID to be excluded from training and used for eval.
+        sample (Sample): Specific sample used in this dataset configuration.
+        _target_ (str): Class identifier for instantiating this dataset.
+    """
+
     split_id: Optional[int] = None
     sample: Sample = MISSING
     _target_: str = "cryovit.data_modules.SingleSampleDataModule"
@@ -96,6 +179,15 @@ class SingleSample(Dataset):
 
 @dataclass
 class MultiSample(Dataset):
+    """Configuration for a dataset involving multiple samples in CryoVIT experiments.
+
+    Attributes:
+        split_id (Optional[int]): Optional split ID for validation.
+        sample (Tuple[Sample]): Tuple of samples used for training.
+        test_samples (Tuple[Sample]): Tuple of samples used for testing.
+        _target_ (str): Class identifier for instantiating this dataset.
+    """
+
     split_id: Optional[int] = None
     sample: Tuple[Sample] = MISSING
     test_samples: Tuple[Sample] = ()
@@ -104,6 +196,15 @@ class MultiSample(Dataset):
 
 @dataclass
 class LOOSample(Dataset):
+    """Leave-One-Out (LOO) dataset configuration for CryoVIT experiments.
+
+    Attributes:
+        split_id (Optional[int]): Optional split ID for validation.
+        sample (Sample): Sample excluded from training (used for testing).
+        all_samples (Tuple[Sample]): Tuple of all samples, including the LOO sample.
+        _target_ (str): Class identifier for instantiating this dataset.
+    """
+
     split_id: Optional[int] = None
     sample: Sample = MISSING
     all_samples: Tuple[Sample] = tuple(s for s in Sample)
@@ -112,6 +213,15 @@ class LOOSample(Dataset):
 
 @dataclass
 class FractionalLOO(Dataset):
+    """Fractional Leave-One-Out (LOO) dataset configuration in CryoVIT experiments.
+
+    Attributes:
+        split_id (int): Number of splits to be used for training.
+        sample (Sample): Sample excluded from training (used for testing).
+        all_samples (Tuple[Sample]): Tuple of all samples, including the LOO sample.
+        _target_ (str): Class identifier for instantiating this dataset.
+    """
+
     split_id: int = MISSING
     sample: Sample = MISSING
     all_samples: Tuple[Sample] = tuple(s for s in Sample)
@@ -120,6 +230,15 @@ class FractionalLOO(Dataset):
 
 @dataclass
 class ExpPaths:
+    """Configuration for managing experiment paths in CryoVIT experiments.
+
+    Attributes:
+        exp_dir (Path): Directory path for saving results from an experiment.
+        tomo_dir (Path): Directory path for tomograms with their DINOv2 features.
+        split_file (Path): Path to the CSV file specifying data splits.
+        cryovit_root (Optional[Path]): Root directory for the CryoVIT package.
+    """
+
     exp_dir: Path
     tomo_dir: Path
     split_file: Path
@@ -128,6 +247,18 @@ class ExpPaths:
 
 @dataclass
 class DataLoader:
+    """Configuration for data loader settings used in CryoVIT experiments.
+
+    Attributes:
+        num_workers (int): Number of worker processes for loading data.
+        prefetch_factor (Optional[int]): Number of batches to prefetch (default is 1).
+        persistent_workers (bool): If True, the data loader will not shutdown workers between epochs.
+        pin_memory (bool): If True, enables memory pinning for faster data transfer to GPU.
+        batch_size (Optional[int]): Number of samples per batch, can be unspecified for default.
+        _target_ (str): Class identifier for instantiating the data loader.
+        _partial_ (bool): Flag to indicate this is a partial configuration.
+    """
+
     num_workers: int = 8
     prefetch_factor: Optional[int] = 1
     persistent_workers: bool = False
@@ -139,6 +270,19 @@ class DataLoader:
 
 @dataclass
 class TrainModelConfig:
+    """Configuration for training a model in CryoVIT experiments.
+
+    Attributes:
+        exp_name (str): The name of the experiment, must be unique for each configuration.
+        label_key (str): Key used to specify the training label: mito or mito_ai.
+        aux_keys (Tuple[str]): Additional keys to load auxiliary data from tomograms.
+        model (Model): The model configuration to be used for training.
+        trainer (TrainerFit): Trainer configuration tailored for training sessions.
+        dataset (Dataset): Dataset configuration to be used for model training.
+        exp_paths (ExpPaths): Configuration paths relevant to the experiment.
+        dataloader (DataLoader): DataLoader configuration for handling input data efficiently.
+    """
+
     exp_name: str = MISSING
     label_key: str = MISSING
     aux_keys: Tuple[str] = ()
@@ -152,6 +296,19 @@ class TrainModelConfig:
 
 @dataclass
 class EvalModelConfig:
+    """Configuration for evaluating a model in CryoVIT experiments.
+
+    Attributes:
+        exp_name (str): The name of the experiment, must be unique for each configuration.
+        label_key (str): Key used to specify the training label: mito or mito_ai.
+        aux_keys (Tuple[str]): Additional keys to load auxiliary data from tomograms.
+        model (Model): The model configuration to be used for evaluation.
+        trainer (TrainerEval): Trainer configuration specifically designed for evaluation sessions.
+        dataset (Dataset): Dataset configuration intended for model evaluation.
+        exp_paths (ExpPaths): Configuration paths relevant to the experiment.
+        dataloader (DataLoader): DataLoader configuration optimized for evaluation scenarios.
+    """
+
     exp_name: str = MISSING
     label_key: str = MISSING
     aux_keys: Tuple[str] = ("data",)
@@ -167,6 +324,8 @@ cs = ConfigStore.instance()
 cs.store(name="dino_features_config", node=DinoFeaturesConfig)
 
 cs.store(group="model", name="cryovit", node=CryoVIT)
+cs.store(group="model", name="unet3d", node=UNet3D)
+
 cs.store(group="trainer", name="trainer_fit", node=TrainerFit)
 cs.store(group="trainer", name="trainer_eval", node=TrainerEval)
 
