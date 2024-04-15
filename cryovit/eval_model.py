19ba7a1c56f8b2572595b78cec4bae54a2d24fbe
Sanket Gupte <6501744+sanketx@users.noreply.github.com>
2024-04-15 03:18:02 -0700

    added docstrings

--- cryovit/eval_model.py
+++ cryovit/eval_model.py
@@ -16,6 +16,17 @@ warnings.simplefilter("ignore")
 
 
 def validate_config(cfg: EvalModelConfig) -> None:
+    """Validates the configuration for evaluating segmentation models on CryoET data.
+
+    Ensures that all necessary parameters are included in the configuration file. If any parameters are
+    missing, it logs an error message and exits the script to prevent further execution.
+
+    Args:
+        cfg (EvalModelConfig): The configuration object containing settings for model evaluation.
+
+    Raises:
+        SystemExit: If any configuration parameters are missing, thereby terminating the script.
+    """
     missing_keys = OmegaConf.missing_keys(cfg)
     error_msg = ["The following parameters were missing from eval_model.yaml"]
 
@@ -33,6 +44,18 @@ def validate_config(cfg: EvalModelConfig) -> None:
     version_base="1.2",
 )
 def main(cfg: EvalModelConfig) -> None:
+    """Main function to orchestrate the evaluation of segmentation models.
+
+    First, it validates the configuration and, if valid, proceeds to run the evaluation process using
+    the specified settings in the config file. It captures and logs any errors encountered during the
+    evaluation process.
+
+    Args:
+        cfg (EvalModelConfig): Configuration object loaded from eval_model.yaml.
+
+    Raises:
+        BaseException: Captures and logs any exceptions that occur during the evaluation of the model.
+    """
     validate_config(cfg)
 
     try:
