c82d8674e6ac6b920d7ff7b55242e8d5a613d339
Sanket Gupte <6501744+sanketx@users.noreply.github.com>
2024-04-15 04:53:21 -0700

    added docstrings for losses and metrics

--- cryovit/models/losses.py
+++ cryovit/models/losses.py
@@ -1,13 +1,28 @@
+"""Loss functions for training CryoVIT segmentation models."""
+
 import torch
+from torch import Tensor
 from torch import nn
 from torchvision.ops import sigmoid_focal_loss
 
 
 class DiceLoss(nn.Module):
-    def __init__(self):
+    """Dice Loss for imbalanced foreground / background segmentation tasks."""
+
+    def __init__(self) -> None:
+        """Initializes the DiceLoss instance."""
         super().__init__()
 
-    def forward(self, y_pred, y_true):
+    def forward(self, y_pred: Tensor, y_true: Tensor) -> Tensor:
+        """Computes the Dice loss between predictions and true values.
+
+        Args:
+            y_pred (Tensor): Predicted probabilities, expected to be logits from the model.
+            y_true (Tensor): Ground truth labels.
+
+        Returns:
+            Tensor: Computed Dice loss value.
+        """
         y_pred = torch.sigmoid(y_pred)
         intersection = torch.sum(y_true * y_pred)
         denom = torch.sum(y_true) + torch.sum(y_pred)
@@ -17,11 +32,27 @@ class DiceLoss(nn.Module):
 
 
 class FocalLoss(nn.Module):
-    def __init__(self, gamma=2):
+    """Focal loss to address class imbalance by focusing more on hard-to-classify instances."""
+
+    def __init__(self, gamma=2) -> None:
+        """Initializes the FocalLoss instance.
+
+        Args:
+            gamma (float, optional): Focusing parameter. Default is 2.
+        """
         super().__init__()
         self.gamma = gamma
 
-    def forward(self, y_pred, y_true):
+    def forward(self, y_pred: Tensor, y_true: Tensor) -> Tensor:
+        """Computes the Focal loss between predictions and true values.
+
+        Args:
+            y_pred (Tensor): Predicted probabilities, expected to be logits from the model.
+            y_true (Tensor): Ground truth labels.
+
+        Returns:
+            Tensor: Computed Focal loss value.
+        """
         weight = (y_true.numel() - y_true.sum()) / y_true.numel()
         return sigmoid_focal_loss(
             y_pred,
--- cryovit/models/metrics.py
+++ cryovit/models/metrics.py
@@ -1,17 +1,33 @@
+"""Metrics for evaluating CryoVIT segmentation models."""
+
 import torch
+from torch import Tensor
 from torchmetrics import Metric
 
 
 class DiceMetric(Metric):
+    """Metric class for calculating the Dice score to evaluate segmentation models."""
+
     higher_is_better = True
 
     def __init__(self, threshold):
+        """Initializes the DiceMetric instance with a threshold for binary classification.
+
+        Args:
+            threshold (float): The threshold to apply to the predictions during Dice score calculation.
+        """
         super().__init__()
         self.thresh = threshold
         self.add_state("dice_score", default=torch.tensor(0.0), dist_reduce_fx="sum")
         self.add_state("total", default=torch.tensor(0.0), dist_reduce_fx="sum")
 
-    def update(self, y_pred, y_true):
+    def update(self, y_pred: Tensor, y_true: Tensor) -> None:
+        """Updates the states for the Dice score computation based on predictions and actual values.
+
+        Args:
+            y_pred (Tensor): Predicted probabilities or logits from the model.
+            y_true (Tensor): Ground truth labels.
+        """
         y_pred = torch.sigmoid(y_pred)
         y_pred = torch.where(y_pred < self.thresh, 0.0, 1.0)
 
@@ -21,5 +37,10 @@ class DiceMetric(Metric):
         self.dice_score += 2 * intersection / (denom + 1e-3)
         self.total += 1
 
-    def compute(self):
+    def compute(self) -> Tensor:
+        """Computes the final Dice score over all updates.
+
+        Returns:
+            Tensor: The average Dice score across all batches.
+        """
         return self.dice_score / self.total
